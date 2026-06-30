ee41150704aaf13b2ba2aa3dd0984c280d82beab
Sehrish Tabsum <sehrishtabsum57@gmail.com>
2026-06-30 08:01:10 +0500

    update user

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import HomePage from './components/HomePage';
 import AddNewUser from './components/AddNewUser';
+import Xyz from './components/Xyz';
 
 function App() {
   return (
     <div className="App">
     <HomePage/>
     <AddNewUser/>
+    <Xyz/>
     </div>
   );
 }
---
+++ src/components/Xyz.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Xyz = () => {
+  return (
+    <div>
+      <h1>update user</h1>
+    </div>
+  )
+}
+
+export default Xyz
